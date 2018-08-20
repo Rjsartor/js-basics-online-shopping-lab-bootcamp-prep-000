b6c1bf83d0cf3bad413bbfdf17ef717923223cb8
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-20 17:47:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
     }
   }
   if (!itemInCart) {
-    console.log("That item is not in your cart.");
+    return ("That item is not in your cart.");
   }
   return cart
 }
