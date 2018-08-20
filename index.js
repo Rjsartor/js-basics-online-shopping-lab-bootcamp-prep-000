355c0fad81a8526d72a2b017c4fe79b323cdcab7
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-20 18:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  let orderTotal = total()
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
