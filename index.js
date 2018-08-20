4f4a2a3c9adbdff7b743162f4ee550b733e1b67f
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-20 18:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    
+    cart = []
     return `Your total cost is $${orderTotal}, which will be charged to the card ${cardNumber}.`
   }
-  cart = []
+  
 }
