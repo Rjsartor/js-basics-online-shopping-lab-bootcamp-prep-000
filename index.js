606c3f34ef4e7ae20d981d4e15d385dabc1524c4
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-20 17:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (cart[i].hasOwnProperty(item)) {
       itemInCart = true;
-     return cart.splice(i, 1);
+     cart.splice(i, 1);
     }
   }
   if (!itemInCart) {
